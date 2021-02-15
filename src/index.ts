83d03d08af91dbb291d2d6e5d9725505f8ff30ea
Denys Vasylenko <denys.vasylenko@ava.codes>
2021-02-16 01:00:49 +0200

    seeds

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 #!/usr/bin / env node
 
 import * as pg from 'pg';
-
+import { readFileSync } from 'fs';
 import { getPostgresPool } from './dbClients/postgres-pool';
 
 main();
 
-async function main() {
+function main() {
   try {
-  } catch (error) {}
+    const data = readFileSync(__dirname + '/input/countries.json', 'utf-8');
+    console.log();
+    console.log(JSON.stringify(Object.values(JSON.parse(data)[0]).map((c: any, i: any) => ({ ...c, id: i }))))
+  } catch (error) {
+    console.log(error);
+  }
 }
