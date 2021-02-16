7522f08843bc9021314d9bc62ef253233aac3aca
Denys Vasylenko <denys.vasylenko@ava.codes>
2021-02-16 04:46:49 +0200

    firebase

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,60 @@
 #!/usr/bin / env node
 
-import * as pg from 'pg';
-import { readFileSync } from 'fs';
-import { getPostgresPool } from './dbClients/postgres-pool';
+import { initializeApp, credential, firestore } from 'firebase-admin';
+initializeApp({
+  credential: credential.cert(__dirname + '/credentials.json'),
+});
+const db = firestore();
 
 main();
 
-function main() {
-  try {
-    const data = readFileSync(__dirname + '/input/countries.json', 'utf-8');
-    console.log();
-    console.log(JSON.stringify(Object.values(JSON.parse(data)[0]).map((c: any, i: any) => ({ ...c, id: i }))))
-  } catch (error) {
-    console.log(error);
-  }
+async function main() {
+
+  // Get User profile
+  const userProfile = await getUserProfile('0');
+  console.log(userProfile);
+
+  // Get User profile hobbies
+  const userProfileHobbies = await getUserProfileHobbies('0');
+  console.log(userProfileHobbies);
+
+  // Get User profile cities
+  const userProfileCities = await getUserProfileCities('0');
+  console.log(userProfileCities);
+
+  // Get User profiles from city
+  const userProfilesFromCity = await getUserProfilesFromCity('Countries/226/Cities/1');
+  console.log(userProfilesFromCity);
+
+  // Get User profiles from same company
+  const userProfilesFromSameCompany = await getUserProfilesFromSameCompany();
+  console.log(userProfilesFromSameCompany);
+}
+
+const getUserProfile = async (userProfileId: string) => {
+  const UserProfileRef = db.collection('UserProfiles');
+  return (await UserProfileRef.doc(userProfileId).get()).data();
+};
+
+const getUserProfileHobbies = async (userProfileId: string) => {
+  const UserProfileRef = db.collection('UserProfiles');
+  const data = (await UserProfileRef.doc(userProfileId).get()).data() || { hobbies: [] };
+  return data.hobbies;
+};
+
+const getUserProfileCities = async (userProfileId: string) => {
+  const UserProfileRef = db.collection('UserProfiles');
+  const data = (await UserProfileRef.doc(userProfileId).get()).data() || { cities: {} };
+  return Object.values(data.cities);
+};
+const getUserProfilesFromCity = async (city: string) => {
+  const UserProfileRef = db.collection('UserProfiles');
+  const data = (await UserProfileRef.where('city', '==', city).get());
+  return data.docs.map(d => d.data());
+};
+
+const getUserProfilesFromSameCompany = async () => {
+  const CompanyUsersRef = db.collection('CompanyUsers');
+  const data = await CompanyUsersRef.get()
+  return data.docs.map(d => d.data());
 }
